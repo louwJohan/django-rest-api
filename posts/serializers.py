a332f5407a1d0088ffa7262c0f755cb31b389b4b
Louw Johan Coetzee <Louwjohanc@gmail.com>
2022-07-22 11:56:43 +0000

    Add extra field to profile

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Post
+from likes.models import Like
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -8,6 +9,7 @@ class PostSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url',
                                               default='../default_post_kqniyi.jpg')
+    like_id = serializers.SerializerMethodField()
 
     def validate_image(self, value):
         if value.size > 1024 * 1024 * 2:
@@ -24,6 +26,15 @@ class PostSerializer(serializers.ModelSerializer):
             )
         return value
 
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Like.objects.filter(owner=user,
+                                        post=obj
+                                                ).first()
+            return like.id if like else None
+        return None
+
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
@@ -33,5 +44,5 @@ class PostSerializer(serializers.ModelSerializer):
         fields = [
             'id', 'owner', 'created_at', 'updated_at', 'title',
             'profile_id', 'is_owner', 'content', 'image',
-            'profile_image', 'image_filter'
+            'profile_image', 'image_filter', 'like_id'
         ]
